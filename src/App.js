63000e76bdc1c868fc66842a4be4835b9574d1e3
Steve Warren <steve@speakaboos.com>
2018-06-02 18:53:41 -0400

    new nav from scratch

--- src/App.js
+++ src/App.js
@@ -1,46 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <a className="navbar-brand" href="#">Navbar</a>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
 
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav mr-auto">
-              <li className="nav-item active">
-                <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Link</a>
-              </li>
+        <div className="nav-container">
 
+          <div className="logo">LOGO</div>
+
+          <nav className="site-nav">
+            <ul>
+              <li><a href="/">Home</a></li>
+              <li><a href="/">Profile</a></li>
+              <li><a href="/">Upload</a></li>
             </ul>
-          </div>
-          <div>
-            <ul className="navbar-nav mr-auto">
-              <li className="nav-item dropdown ml-auto">
-                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                  Dropdown
-                </a>
-                <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <a className="dropdown-item" href="#">Action</a>
-                  <a className="dropdown-item" href="#">Another action</a>
-                  <div className="dropdown-divider"></div>
-                  <a className="dropdown-item" href="#">Something else here</a>
-                </div>
-              </li>
-            </ul>
-          </div>
-        </nav>
+          </nav>
 
+          <div className="login">
+            <a href="/">LOGIN</a>
+          </div>
 
+        </div>
 
       </div>
     );
