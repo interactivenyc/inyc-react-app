4dffd25b71b651664b3f2f3c3a2c5a72885ef543
Steve Warren <steve@speakaboos.com>
2018-06-26 08:50:55 -0400

    trying to colorize png with css

--- src/components/HamburgerMenu.js
+++ src/components/HamburgerMenu.js
@@ -1,10 +1,11 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import '../css/HamburgerMenu.css';
+import HamburgerPNG from '../images/hamburger_black.png';
 
 function clickOutside(event) {
-  if (!event.target.matches('.example-button')) {
-    const dropdowns = document.getElementsByClassName('example-dropdown-content');
+  if (!event.target.matches('.hamburger-button')) {
+    const dropdowns = document.getElementsByClassName('hamburger-dropdown-content');
     let i;
     for (i = 0; i < dropdowns.length; i += 1) {
       const openDropdown = dropdowns[i];
@@ -18,17 +19,31 @@ function clickOutside(event) {
 window.addEventListener('click', clickOutside);
 
 function onClickExample() {
-  // document.getElementById('example-dropdown').classList.toggle('show');
+  document.getElementById('hamburger-dropdown').classList.toggle('show');
   console.log('fuck');
 }
 
 const HamburgerMenu = () => (
   <div className="hamburger-menu">
+    <div className="hamburger-icon">
+      <img src={HamburgerPNG} className="hamburger-icon" alt="menu icon" />
+    </div>
 
-    <div role="button" onClick={onClickExample}>
-      <div className="hamburger" />
-      <div className="hamburger" />
-      <div className="hamburger" />
+    <div role="button" onClick={onClickExample} className="hamburger-menu-icon">
+      <div className="hamburger-menu-bg">
+        <div className="hamburger-lines" />
+        <div className="hamburger-lines" />
+        <div className="hamburger-lines" />
+      </div>
+    </div>
+
+    <div id="hamburger-dropdown" className="hamburger-dropdown-content">
+      <Link to="/ImageOverlay">ImageOverlay</Link>
+      <Link to="/StyledTable">StyledTable</Link>
+      <Link to="/StyledForm">StyledForm</Link>
+      <Link to="/DropdownMenu">DropdownMenu</Link>
+      <Link to="/ResponsiveGrid">ResponsiveGrid</Link>
+      <Link to="/Slideshow">Slideshow</Link>
     </div>
 
   </div>
